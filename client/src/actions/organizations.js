157e72892e41ff3fd0cdf902f8f8be5307474678
KevinMAnderson <kma2k4@hotmail.com>
2018-02-05 14:00:00 -0700

    created const dbUrl and used template literal with this const in the CRUD actions in this file so can just change the base dburl and the CRUD actions will be correct...functionally the same

--- client/src/actions/organizations.js
+++ client/src/actions/organizations.js
@@ -1,122 +1,116 @@
-import axios from 'axios'
+import axios from 'axios';
 
+const RECEIVE_ALL_ORGANIZATIONS = 'RECEIVE_ALL_ORGANIZATIONS';
+const RECEIVE_ADDED_ORGANIZATION = 'RECEIVE_ADDED_ORGANIZATION';
+const RECEIVE_UDPATED_ORGANIZATION = 'RECEIVE_UDPATED_ORGANIZATION';
+const RECEIVE_ORGANIZATIONS_BY_LOCATION = 'RECEIVE_ORGANIZATIONS_BY_LOCATION';
 
-const RECEIVE_ALL_ORGANIZATIONS = 'RECEIVE_ALL_ORGANIZATIONS'
-const RECEIVE_ADDED_ORGANIZATION = 'RECEIVE_ADDED_ORGANIZATION'
-const RECEIVE_UDPATED_ORGANIZATION = 'RECEIVE_UDPATED_ORGANIZATION'
-const RECEIVE_ORGANIZATIONS_BY_LOCATION = 'RECEIVE_ORGANIZATIONS_BY_LOCATION'
-
+const dbUrl = 'http://localhost:3000/api/';
 
 // Pull all accepted organizations
-const receiveAllOrganizations = (data) => {
+const receiveAllOrganizations = data => {
 	return {
 		type: RECEIVE_ALL_ORGANIZATIONS,
 		organizations: data
-	}
-}
+	};
+};
 
 const getAllOrganizations = () => {
-	return (dispatch) => {
-		return axios.get('http://localhost:4000/api/organizations')
-		.then(
-			response => response.data,
-			error => console.log('A request error occurred', error)
-		)
-		.then(
-			data => dispatch(receiveAllOrganizations(data))
-		)
-	}
-}
+	return dispatch => {
+		return axios
+			.get(`${dbUrl}organizations`)
+			.then(
+				response => response.data,
+				error => console.log('A request error occurred', error)
+			)
+			.then(data => dispatch(receiveAllOrganizations(data)));
+	};
+};
 
 // Pull accepted organizations by location search
-const receiveOrganizationsByLocation = (data) => {
+const receiveOrganizationsByLocation = data => {
 	return {
 		type: RECEIVE_ORGANIZATIONS_BY_LOCATION,
 		organizations: data
-	}
-}
+	};
+};
 
 const getOrganizationsByLocation = (address, distance) => {
-	console.log('in action', address, distance)
-	return (dispatch) => {
-		return axios.get('http://localhost:4000/api/organization', { 
-			params: {
-			  address,
-			  distance
-			}
-		})
-		.then(
-			response => response.data,
-			error => console.log('A request error occurred', error)
-		)
-		.then(
-			data => dispatch(receiveOrganizationsByLocation(data))
-		)
-	}
-}
-
+	console.log('in action', address, distance);
+	return dispatch => {
+		return axios
+			.get(`${dbUrl}organizations`, {
+				params: {
+					address,
+					distance
+				}
+			})
+			.then(
+				response => response.data,
+				error => console.log('A request error occurred', error)
+			)
+			.then(data => dispatch(receiveOrganizationsByLocation(data)));
+	};
+};
 
 // Create new organization
-const receiveAddedOrganization = (data) => {
+const receiveAddedOrganization = data => {
 	return {
 		type: RECEIVE_ADDED_ORGANIZATION,
 		organization: data
-	}
-}
+	};
+};
 
-const addOrganization = ({ 
-  name = '',
-  category = '',
-  description = '',
-  address = ''
+const addOrganization = ({
+	name = '',
+	category = '',
+	description = '',
+	address = ''
 }) => {
-	return (dispatch) => {
-		return axios.post('http://localhost:4000/api/organization', {
-			name,
-			category,
-			description,
-			address
-		})
-		.then(
-			response => response.data,
-			error => console.log('A request error occurred', error)
-		)
-	}
-}
+	return dispatch => {
+		return axios
+			.post(`${dbUrl}organization`, {
+				name,
+				category,
+				description,
+				address
+			})
+			.then(
+				response => response.data,
+				error => console.log('A request error occurred', error)
+			);
+	};
+};
 
 // Update organization
-const receiveUpdatedOrganization = (updates) => {
+const receiveUpdatedOrganization = updates => {
 	return {
 		type: RECEIVE_UDPATED_ORGANIZATION,
 		updates
-	}
-}
+	};
+};
 
-const updateOrganization = (updates) => {
-	return (dispatch) => {
-		return axios.put('http://localhost:4000/api/updateorganization', updates)
-		.then(
-			response => response.data,
-			error => console.log('A request error occurred', error)
-		)
-		.then(
-			data => dispatch(receiveUpdatedOrganization(data[0]))
-		)
-	}
-}
+const updateOrganization = updates => {
+	return dispatch => {
+		return axios
+			.put(`${dbUrl}updateorganization`, updates)
+			.then(
+				response => response.data,
+				error => console.log('A request error occurred', error)
+			)
+			.then(data => dispatch(receiveUpdatedOrganization(data[0])));
+	};
+};
 
-
-
-export { 
-	getAllOrganizations, 
-	RECEIVE_ALL_ORGANIZATIONS, 
+export {
+	getAllOrganizations,
+	RECEIVE_ALL_ORGANIZATIONS,
 	getOrganizationsByLocation,
 	RECEIVE_ORGANIZATIONS_BY_LOCATION,
 	addOrganization,
 	receiveAddedOrganization,
-	RECEIVE_ADDED_ORGANIZATION, 
-	updateOrganization, 
-	RECEIVE_UDPATED_ORGANIZATION, 
-	receiveUpdatedOrganization,
-}
-
+	RECEIVE_ADDED_ORGANIZATION,
+	updateOrganization,
+	RECEIVE_UDPATED_ORGANIZATION,
+	receiveUpdatedOrganization
+};
