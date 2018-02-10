ba58b468666a41dfece98828a920bfa98effb521
KevinMAnderson <kma2k4@hotmail.com>
2018-02-09 17:39:52 -0700

    updated header to be styled for mobile and desktop and changed validation ui....still have a glitch in the validation ui

--- client/src/components/LocationSearch.js
+++ client/src/components/LocationSearch.js
@@ -8,24 +8,17 @@ import LocationSearchForm from './LocationSearchForm'
 import { setMapCenter } from '../actions/userControl'
 import { getOrganizationsByLocation } from '../actions/organizations'
 
-
 class LocationSearch extends React.Component {
-	handleSubmit = (search) => {
+	handleSubmit = search => {
 		this.props.dispatch(getOrganizationsByLocation(search.address))
 		geocodeAddress(search.address)
 			.then(latLng => this.props.dispatch(setMapCenter(latLng)))
 			.then(() => this.props.history.push('/maps'))
-			this.props.dispatch(reset('locationSearchForm'))
+		this.props.dispatch(reset('locationSearchForm'))
 	}
 
 	render() {
-		return (
-			<div>
-				<LocationSearchForm 
-					onSubmit={this.handleSubmit}
-				/>
-			</div>
-		)
+		return <LocationSearchForm onSubmit={this.handleSubmit} />
 	}
 }
 
