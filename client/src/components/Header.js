3b7c307c388688f2f2c6278837639945936a36b6
Fred Wehle <fredmon3@gmail.com>
2018-09-09 16:53:42 -0600

    material-ui and header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,112 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { Menu, Search } from '@material-ui/icons'
+import { AppBar, Toolbar, Typography, IconButton, Input } from '@material-ui/core';
 
-const Header = () => (
-	<div>
-		<p>Kalicos</p>
-	</div>
-)
+const styles = (theme) => ({
+	root: {
+		width: '100%',
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20,
+	},
+	title: {
+		display: 'none',
+		[theme.breakpoints.up('sm')]: {
+			display: 'block',
+		},
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25),
+		},
+		marginRight: theme.spacing.unit * 2,
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing.unit * 3,
+			width: 'auto',
+		},
+	},
+	searchIcon: {
+		width: theme.spacing.unit * 9,
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	inputRoot: {
+		color: 'inherit',
+		width: '100%',
+	},
+	inputInput: {
+		paddingTop: theme.spacing.unit,
+		paddingRight: theme.spacing.unit,
+		paddingBottom: theme.spacing.unit,
+		paddingLeft: theme.spacing.unit * 10,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: 200,
+		},
+	},
+	sectionDesktop: {
+		display: 'none',
+		[theme.breakpoints.up('md')]: {
+			display: 'flex',
+		},
+	},
+	sectionMobile: {
+		display: 'flex',
+		[theme.breakpoints.up('md')]: {
+			display: 'none',
+		},
+	},
+})
 
-export default Header
+const Header = (props) => {
+	const { classes } = props
+	return (
+		<AppBar>
+			<Toolbar>
+				<IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+					<Menu />
+				</IconButton>
+				<Typography className={classes.title} variant="title" color="inherit" noWrap>
+					Kalicos
+				</Typography>
+				<div className={classes.grow} />
+				<div className={classes.search}>
+					<div className={classes.searchIcon}>
+						<Search />
+					</div>
+					<Input
+						placeholder="Search…"
+						disableUnderline
+						classes={{
+							root: classes.inputRoot,
+							input: classes.inputInput,
+						}}
+					/>
+				</div>
+			</Toolbar>
+		</AppBar>
+	)
+}
+
+Header.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
